f56f2f5e20f7d85d666b2e2cea10f354d1f18dcf
Alin <alingabriel743@yahoo.ro>
2025-03-15 19:37:30 +0200

    Refactored

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ import sys
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 PRODUCTION = st.secrets['PRODUCTION']
+
 st.set_page_config(
     page_title="Streamlit RAG App",
     page_icon="💬",
@@ -24,39 +26,38 @@ def validate_api_key(api_key):
         return False
 
 def main():
-    st.title("Bine ati venit in aplicatie 💬")
+    st.title("Welcome to the Application 💬")
     st.write("""
-        Aceasta aplicatie are doua functionalitati principale:
-        1. Chat simplu: interactionati cu LLM-ul intr-un mod simplu, conversational.
-        2. Chat cu fisiere PDF: incarcati un fisier PDF si interactionati cu LLM-ul pentru a extrage informatii din el.
+        This application has two main functionalities:
+        1. **Simple Chat**: Interact with the LLM in a simple, conversational way.
+        2. **Chat with PDF files**: Upload a PDF file and interact with the LLM to extract information from it.
     """)
-
 
     is_production = PRODUCTION == "True"
 
     if is_production:
-        st.subheader("Introduceti cheia API OpenAI")
+        st.subheader("Enter OpenAI API Key")
         
         if "api_key" not in st.session_state:
             st.session_state.api_key = None
         api_key_input = st.text_input(
-            "Cheia API OpenAI",
+            "OpenAI API Key",
             type="password",
-            placeholder="Introduceti cheia dvs. API",
+            placeholder="Enter your API key",
         )
 
-        if st.button("Valideaza cheia API"):
+        if st.button("Validate API Key"):
             if validate_api_key(api_key_input):
                 st.session_state.api_key = api_key_input
-                st.success("Cheia API este valida! Navigati la functionalitatile aplicatiei.")
+                st.success("The API key is valid! Navigate to the application functionalities.")
             else:
-                st.error("Cheia API nu este valida. Incercati din nou.")
+                st.error("The API key is not valid. Please try again.")
         if not st.session_state.api_key:
-            st.warning("Introduceti o cheie API valida pentru a accesa functionalitatile aplicatiei.")
+            st.warning("Please enter a valid API key to access the application's functionalities.")
         else:
-            st.info("Cheia API este configurata. Puteti naviga la paginile aplicatiei.")
+            st.info("The API key is configured. You can now navigate to the application pages.")
     else:
-        st.info("Mod dezvoltare: toate paginile sunt disponibile fara autentificare API.")
+        st.info("Development mode: All pages are available without API authentication.")
 
 if __name__ == "__main__":
     main()
